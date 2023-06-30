127f9e8b88b0216b42971d91bb2dd2c32907a6a7
LcErhel <132855195+LcErhel@users.noreply.github.com>
2023-06-30 14:26:43 +0200

    basic operate

--- javascript.js
+++ javascript.js
@@ -4,6 +4,28 @@
 
 //
 
+let firstOperand = 12;
+let secondOperand = 4;
+let operator = "/";
+
+function operate(a, b, op) {
+    if (op == "+") {
+        return add(a, b);
+    } else if (op == "-") {
+        return subtract(a, b);
+    } else if (op == "*") {
+        return multiply(a, b);
+    } else if (op == "/") {
+        return divide(a, b);
+    } else {
+        return console.log("operate broken");
+    }
+}
+
+console.log(operate(firstOperand, secondOperand, operator));
+
+//
+
 function add(a, b) {
     return a + b;
 }
