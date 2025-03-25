cce5dfbf81dd76c2571707248e4c327db72fb07d
project <132855195+LcErhel@users.noreply.github.com>
2025-03-25 18:02:47 +0300

    add multiple functions and eventListeners, calculator works in current condition

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 "use strict";
 
-let num1 = 32;
-let num2 = 13;
+let onFirstNum = true;
+let num1 = 0;
+let num2 = 0;
 let operator = "add";
+let display = document.querySelector(".numbersDisplay");
+let evaluate = document.querySelector("#eval");
+let showOperator = document.querySelector(".showOperator");
 
 let cases = document.querySelectorAll(".case");
 console.log(cases);
 cases.forEach(element => {
     element.addEventListener("click", () => {
         operator = element.id;
+        onFirstNum = false;
+        refreshOperatorDisplay()
         console.log(operator);
     });
 });
@@ -16,8 +22,12 @@ cases.forEach(element => {
 let digits = document.querySelectorAll(".digit");
 digits.forEach(element => {
     element.addEventListener("click", () => {
-        operator = element.id;
-        console.log(operator);
+        // num1 == 0 ? num1 = element.id : num1 += element.id;
+        if(onFirstNum) {
+            getFirstNumber(element);
+        } else {
+            getSecondNumber(element);
+        }
     });
 });
 console.log(digits);
@@ -36,7 +46,15 @@ function operate(a, b, op) {
             console.log("operate() failed");
             break;
     }
+
 }
+
+evaluate.addEventListener("click", () => {
+    num1 = operate(+num1, +num2, operator);
+    num2 = 0;
+    display.textContent = num1;
+    console.log("check");
+});
 
 function add(a, b) {
     return a + b;
@@ -52,4 +70,18 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function refreshOperatorDisplay() {
+    showOperator.textContent = operator;
+}
+
+function getFirstNumber(element) {
+    num1 == 0 ? num1 = element.id : num1 += element.id;
+    display.textContent = num1;
+}
+
+function getSecondNumber(element) {
+    num2 == 0 ? num2 = element.id : num2 += element.id;
+    display.textContent = `${num1} || ${num2}`;
 }
